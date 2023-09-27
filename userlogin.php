fa94d0fa022f882b201c9d4cc80cb0e674c6f95c
F04C <israeldarrel@yahoo.com>
2023-09-27 20:36:01 +0800

    added userlogin

---
+++ userlogin.php
@@ -0,0 +1,52 @@
+<?php
+    require 'db_conn.php';   
+
+    if(isset($_POST['btnLogin']))
+    {
+
+            $usernameInput = $_POST['inputUsername'];
+            $userpassInput = $_POST['inputPassword'];
+
+            $sql = "SELECT * FROM tbluser  where username = '".$usernameInput."' and password = '".$userpassInput."';";
+            if($conn)
+            {
+                try{
+                    $executeSQL = mysqli_query($conn, $sql);
+                    
+                    if($executeSQL)
+                    {
+                        $numRows = mysqli_num_rows($executeSQL);
+                        if($numRows == 1)
+                        {
+                            $record = mysqli_fetch_assoc($executeSQL);
+
+                            session_start();
+                            $_SESSION['username'] = $record['username'];
+                            $_SESSION['userid'] = $record['idtblusers'];
+
+                            header('Location: index.php');
+                        }
+                        else
+                        {
+                            echo "No user found!";
+                        }
+                    }
+                    else
+                    {
+                        echo "There is an error. Please try again";
+                    }
+                }catch(Exception $e)
+                {
+                    echo 'Error' . $e;
+                }
+            
+            } 
+                else
+            {
+            }
+    }
+    else{
+        echo "";
+    }
+
+?>
