e8a93706b7f51a5f835abd856d7332a8b1a45078
F04C <israeldarrel@yahoo.com>
2023-09-29 19:17:06 +0800

    added work

--- register.php
+++ register.php
@@ -1,3 +1,10 @@
 <?php
+    //dbconn for registration
 
+    //query for database
+
+    //pop up if the user has registered
+    //pop up if there is an error
+    //pop up if there is a duplicate
+    //pop up if there is no database
 ?>
