5b4088b6c7c6d72c39eb965f16a59708ecbe9767
F04C <israeldarrel@yahoo.com>
2023-09-30 01:32:50 +0800

    added new files. check the comments

---
+++ medicine.php
@@ -0,0 +1,19 @@
+<?php
+        require 'dbconn.php';
+
+        $prescriptionQuery
+
+        //add code to get the list of available medicine in the database
+        //if there is no available medicine, a doctor can put the name of the medicine
+        //on the textbox and it will automatically appear on the prescription textbox
+        //and it will be automatically added to the database
+
+        //add code for the duration of the treatment
+        //add code for the strength/dosage of each medicine in the database
+        //all of the above code will be automatically added to the database if it is not already present
+
+
+        //add code if there will be a follow up treatment
+        //show the status of the appointment whether it is a follow up treatment, checkup treatment, or the likes.
+        
+?>
