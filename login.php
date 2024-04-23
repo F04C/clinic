dcdbe6715ec9ed682f95c61c0a45885505be24fe
F04C <israeldarrel@yahoo.com>
2024-04-24 02:29:59 +0800

    updated

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<!Doctype html>
+<!doctype html>
 <html lang="en">
 
 <head>
@@ -12,7 +12,7 @@
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
-	<link rel="stylesheet" href="loginassets/css/style.css">
+	<link rel="stylesheet" href="/loginassets/css/style.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script src="loginassets\js\1.js"></script>
 </head>
