c9bed24d8acd8f064c8cc18fbd25ed14e43806fd
Darrel Israel <israeldarrel@yahoo.com>
2024-06-06 10:22:42 +0800

    formatted

--- loginassets/js/login.js
+++ loginassets/js/login.js
@@ -1,34 +1,34 @@
 // Add this code to your 1.js file
-$(document).ready(function() {
-    $(".toggle-password").click(function() {
-        $(this).toggleClass("fa-eye fa-eye-slash");
-        var input = $($(this).attr("toggle"));
-        
-        if (input.attr("type") === "password") {
-            input.attr("type", "text");
-        } else {
-            input.attr("type", "password");
-        }
-    });
+$(document).ready(function () {
+  $(".toggle-password").click(function () {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+
+    if (input.attr("type") === "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
 });
 
 // Function to check if the user exists
 function checkUserExists() {
-    if (!userExists) {
-        showUserNotFoundAlert();
-        showContactAdminMessage(); // Function to display the h5 element
-        return false; // Prevent form submission
-    }
+  if (!userExists) {
+    showUserNotFoundAlert();
+    showContactAdminMessage(); // Function to display the h5 element
+    return false; // Prevent form submission
+  }
 }
 
 // Function to display an alert when no user is found
 function showUserNotFoundAlert() {
-    alert("User not found. Please check your username and password.");
+  alert("User not found. Please check your username and password.");
 }
 
 // Function to display the h5 element
 function showContactAdminMessage() {
-    $("#contactAdminMsg").removeClass("d-none"); // Remove the 'd-none' class to make it visible
+  $("#contactAdminMsg").removeClass("d-none"); // Remove the 'd-none' class to make it visible
 }
 
 // Variable to check if the user exists
@@ -36,5 +36,5 @@ var userExists = false; // Set to false if the user is not found
 
 // Check if the user exists
 if (!userExists) {
-    showContactAdminMessage(); // Display the message when the user is not found
+  showContactAdminMessage(); // Display the message when the user is not found
 }
